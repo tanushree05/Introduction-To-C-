c022b8e40b114b2476472fde1567ce3317463bb9
tanushree05 <singhtanushree111@gmail.com>
2024-05-02 17:02:13 +0530

    [tanushree]added program for type casting

--- Programfile.cs
+++ Programfile.cs
@@ -5,6 +5,33 @@ using System.Text;
 namespace program.Introduction_To_C_Sharp
 {
     internal class Programfile
+
     {
+        static void Main()
+        {
+            int a = 10;
+            float b = a;
+                                        //implicit - int to float
+            Console.WriteLine(a);//10
+            Console.WriteLine(b);//10
+
+            double c = 10.99;
+                              //explicit -double to int 
+            int d = (int)c;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+
+            //typecsting methods
+
+           Console.WriteLine("Enter age :");//initially the program take value in String 
+
+            int age= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Age="+age);
+            
+
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Introduction-To-C#
+# C# Type Casting
+# Type casting is when you assign a value of one data type to another type.
+
+# In C#, there are two types of casting:
+
+# Implicit Casting (automatically) - converting a smaller type to a larger type size
+# char -> int -> long -> float -> double
+
+# Explicit Casting (manually) - converting a larger type to a smaller size type
+# double -> float -> long -> int -> char
+
+
+
+# Type Conversion Methods
+# It is also possible to convert data types explicitly by using built-in methods, 
+# such as Convert.ToBoolean, Convert.ToDouble, Convert.ToString, Convert.ToInt32 (int) and Convert.ToInt64 (long):
+
